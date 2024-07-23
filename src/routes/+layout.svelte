f884030a0313e555687183732e20a45e7c767a6b
Dimitrije Jovanovic <20dimitrije02@gmail.com>
2024-07-23 19:56:59 +0300

    add basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { enhance } from '$app/forms';
+
+  export let data
+</script>
+
+<div class="header">
+  <div class="items">
+    <a href="/">Geek House</a>
+    <a href="/">Home</a>
+    <a href="/about">About us</a>
+    <a>Games</a>
+    <a>Shop</a>
+    <a>Contact us</a>
+  </div>
+  {#if data.user}
+    <div>
+      <form action="/logout" method="POST" use:enhance>
+        <button type="submit">Log out</button>      
+      </form>
+    </div>
+  {:else}
+    <div class="items">
+      <a href="/login">Log in</a>
+      <a href="/register">Register</a>
+    </div>
+  {/if}
+</div>
+<slot />
+
+<style>
+  .header {
+    display: flex;
+    margin: 16px 32px;
+    justify-content: space-between;
+  }
+  .items {
+    display: flex;
+    gap: 16px;
+  }
+</style>
